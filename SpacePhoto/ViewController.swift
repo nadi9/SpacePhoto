9d0e63bb7a28c01300be7d5f7f193c7e926081c9
tam3rlan <nadia@macbook-pro-2.local>
2018-12-22 09:54:28 +0300

    main UI elements added

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    @IBOutlet weak var imageDescription: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
